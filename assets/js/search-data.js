a65dd688990fb9951a193b262ca70f49b76f1c82
haoyang5 <91243135+haoyang5@users.noreply.github.com>
2026-02-27 21:41:00 +0000

    Deploying to gh-pages from @ haoyang5/howard@518d01bb1022cf5e37ac51d7dcdb6ba7e45031b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -67,9 +67,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-esfa",
+          title: 'ESFA',
+          description: "Efficiently large point FFT Acceleration on Versal ACAP architecture",
           section: "Projects",handler: () => {
               window.location.href = "/howard/projects/1_project/";
             },},{id: "projects-project-2",
