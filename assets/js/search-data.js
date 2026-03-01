469d6b4a2fbef0d4550ae0f89551a2f82280f569
haoyang5 <91243135+haoyang5@users.noreply.github.com>
2026-03-01 19:52:30 +0000

    Deploying to gh-pages from @ haoyang5/howard@ce43cc46bc5dd4523b402a309091ab00df9c574d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,14 +39,14 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/howard/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/howard/teaching/";
